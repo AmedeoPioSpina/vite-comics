a6de9b35d806475ad547dcee0fbc07ff7513f127
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2023-12-14 22:42:28 +0100

    commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template lang="">
+  <AppHeader />
+  <!-- <AppMain/>
+  <AppFooter/> -->
+</template>
+
+<script>
+  import AppHeader from "./components/Appheader.vue";
+
+  export default {
+    components: {
+      AppHeader,
+    }
+  }
+</script>
+
+<style lang="">
+  
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,39 @@
+<template lang="">
+   <section>
+    <nav>
+        <div>
+            <img src="../assets/img/dc-logo.png" alt="">
+        </div>
+        <ul>
+            <li>
+            </li>
+        </ul>
+    </nav>
+   </section>
+</template>
+
+<script>
+export default {
+    name: "AppHeader",
+    data() {
+        return {
+            linksNamesList: [
+                "CHARACTERS",
+                "COMICS",
+                "MOVIES",
+                "TV",
+                "GAMES",
+                "COLLECTIBLES",
+                "VIDEOS",
+                "FANS",
+                "NEWS",
+                "SHOP"
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="">
+    
+</style>
